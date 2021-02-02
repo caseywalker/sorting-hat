7c569050f1cda33956a6a59494ee79348b0c877c
Caseywalker <casey.walker118@gmail.com>
2021-02-01 21:26:30 -0600

    Added expel function, and required form input

--- main.js
+++ main.js
@@ -18,13 +18,17 @@ const studentBuilder = (taco) => {
     <div class="card-body">
       <h5 class="card-title">${student.name}</h5>
       <p class="card-text">${student.house}</p>
-      <button type="button" class="btn btn-secondary" id="expel">Expel</button>
+      <button type="button" class="btn btn-secondary" id="${student.id}">Expel</button>
       </div> 
     </div>`;
   }
   printToDom('#studentDiv', domString)
 }
 
+const warningMessage = () => {
+  messageString = '<h3>Please type in a name</h3>'
+  printToDom('#warningMessage', messageString)
+};
 //** Event Handlers **//
 
 const newStudent = (e) => {
@@ -36,17 +40,24 @@ const newStudent = (e) => {
 
   // Target form values //
   const studentName = document.querySelector('#inputStudentName').value;
+  // Create unique ID for students 
+  const studentIds = students.map(student => student.id).sort((a,b) => a - b);
 
+  const id = studentIds.length ? studentIds[(studentIds.length - 1)] + 1 : 1;
   // Store values in obj //
   const obj = {
     name: studentName,
-    house: housePicker
+    house: housePicker,
+    id: id
   };
-
+  if (studentName.length === 0) {
+    warningMessage();
+  } else {
   // Update data // 
   students.push(obj);
   // Update DOM //
   studentBuilder(students);
+  };
   // Reset the form //
   document.querySelector('form').reset();
   // Event Listener for 'expel' button //
@@ -61,7 +72,7 @@ const handleButtonClick = (e) => {
       <div class="row mb-3 text-center">
         <label for="inputStudentName" class="col-sm-2 col-form-label">Student:</label>
         <div class="col-sm-4">
-          <input type="text" class="form-control" id="inputStudentName" style="width: 300px" required="true">
+          <input type="text" class="form-control" id="inputStudentName" style="width: 300px">
         </div>
         <button type="submit" class="btn btn-secondary col-sm-2">Submit</button>
         </form>
@@ -73,12 +84,12 @@ const handleButtonClick = (e) => {
 };
 const expelStudent = (e) => {
     const targetType = e.target.type;
-    const targetId = e.target.id;
-
+    const targetId = Number(e.target.id);
     if (targetType === 'button') {
-    students.splice(targetId, 1)
+    const studentIndex = students.findIndex(student => student.id === targetId);
+    students.splice(studentIndex, 1);
     }
-    studentBuilder(students)
+    studentBuilder(students);
 }
 
 // ** Event Listeners **//
